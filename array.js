04efcb767e977f5e97da3811289388d617e9540f
ALYANSHEIKHH <alyansheikhh123@gmail.com>
2024-04-21 02:04:05 +0500

    push to git hub

---
+++ array.js
@@ -0,0 +1,8 @@
+var my_cart = ["Rice", "Wheat", "oil"];
+console.log("here is my cart third", my_cart[2]);
+// my_cart[3]="sugar"
+// console.log(my_cart)
+// my_cart[4]="daal"
+// console.log(my_cart)  
+my_cart.push("sugar");
+console.log(my_cart);
---
+++ array.ts
@@ -0,0 +1,9 @@
+const my_cart:Array<string>=["Rice","Wheat","oil"];
+console.log("here is my cart third" ,my_cart[2])
+my_cart[3]="sugar"
+console.log(my_cart)
+my_cart[4]="daal"
+console.log(my_cart)  
+my_cart.push("sugar")
+console.log(my_cart);
+ 
